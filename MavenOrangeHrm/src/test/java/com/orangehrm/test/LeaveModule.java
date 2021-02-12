0f416e62126230debeb4603688942d29849f8e1b
BTM-FACULTY <BTM-FACULTY@DESKTOP-S16N51Q>
2021-02-12 13:52:09 +0530

    added new test case

--- MavenOrangeHrm/src/test/java/com/orangehrm/test/LeaveModule.java
+++ MavenOrangeHrm/src/test/java/com/orangehrm/test/LeaveModule.java
@@ -1,5 +1,13 @@
 package com.orangehrm.test;
 
-public class LeaveModule {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
+import com.orangehrm.generics.BaseClass;
+
+public class LeaveModule extends BaseClass{
+@Test
+public void testCofigureLeaves() {
+	Reporter.log("CofigureLeaves",true);
+}
 }
